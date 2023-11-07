1191c51acba92dd7d5ab8351459af1730705ac3d
Babu <neupanebabu828@gmail.com>
2023-11-07 16:44:02 +1030

    initial setup

---
+++ ui/src/App.tsx
@@ -0,0 +1,45 @@
+import {useEffect, useState} from "react";
+import axios, {AxiosError, CanceledError} from "axios";
+interface Employee {
+    id: number;
+    name: string;
+    role: string;
+}
+const App = () => {
+    const [employees, setEmployees] = useState<Employee[]>([]);
+    const [error, setError] = useState('');
+    const [isLoading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const controller = new AbortController();
+        setLoading(true);
+        const fetchCustomer = async () => {
+            try {
+                const res = await axios.get<Employee[]>('http://localhost:8080/employees', {signal: controller.signal})
+                console.log(res.data);
+                setEmployees(res.data);
+                setLoading(false);
+            } catch (err) {
+                if (err instanceof CanceledError) return;
+                setError((err as AxiosError).message);
+                setLoading(false);
+            }
+        }
+        fetchCustomer();
+        return () => {
+            controller.abort();
+        };
+    }, []);
+    return (
+        <>
+            {error && <p>No data</p>}
+            { isLoading && <div className="spinner-border"></div>}
+            <ul>
+                {employees.map(e => <li key={e.id}>{e.name}</li>
+                )}
+            </ul>
+        </>
+    );
+};
+
+export default App;
