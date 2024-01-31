69c5c2f255ee81b27d58d8485148eac4c8929939
Baburam Neupane <neupanebabu828@gmail.com>
2024-01-31 15:34:49 +0545

    show welcome text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,22 @@
 import {useEffect, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {default as axios} from "axios";
 import {AxiosResponse} from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [welcome, setWelcome] = useState("");
 
     useEffect(() => {
         const welcomeUrl = "http://localhost:8080/room-to-rent/welcome"
         axios.get(welcomeUrl).then(function (response: AxiosResponse) {
+            setWelcome(response.data);
             console.count(response.data);
         })
     }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{welcome}</h1>
     </>
   )
 }
